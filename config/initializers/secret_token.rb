5d99236a76c5d1040b0137b4b1ff86db91ce643a
jameskyokushin <james@kyokushin.com.ph>
2012-01-04 16:51:29 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inquirycall::Application.config.secret_token = '55fee67ef0ee85d197f3ce06b61a4d8cca8b6ec4a3dbab4f7856bb3b647c00b91da4d5dd6f5fa2e53b1116a046d78b0e950bc7178af6b0e2086c559ed8682bc9'
